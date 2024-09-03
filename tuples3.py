a2a0edd40b3608fa6fcef9412bca5f78de935044
spikenardJ <jennifer_annette@hotmail.com>
2024-09-02 23:38:07 -0500

    Third commit

--- tuples3.py
+++ tuples3.py
@@ -6,28 +6,29 @@ import sys
 from termcolor import colored
 
 def add_new_customer_order(orders):
-    for new_order in orders:
-        customer_name = input("Enter the name of the customer: ").title()
-        product_ordered = input(f"Enter the product that {customer_name} ordered: ").title()
-        quantity = input(f"Enter the quantity of {product_ordered} that {customer_name} ordered: ")
-        new_order = (customer_name, product_ordered, quantity)
 
-        if new_order in orders:
-            print("This order already exists.")
-            return
-        else:
-            orders.append((new_order))
-            print(f"{customer_name}'s {product_ordered}(s) has been added to orders successfully.")
-            return
+        try:
+            customer_name = input("Enter the name of the customer: ").title()
+            product_ordered = input(f"Enter the product that {customer_name} ordered: ").title()
+            quantity = int(input(f"Enter the quantity of {product_ordered} that {customer_name} ordered: "))
+            new_order = (customer_name, product_ordered, quantity)
+            if new_order in orders:
+                print("This order already exists.")
+                return
+            else:
+                orders.append(new_order)
+                print(f"{customer_name}'s {product_ordered}(s) has been added to orders successfully.")
+                return
+        except ValueError:
+            print("Invalid input. Quantity must be a number.")
 
 def display_customer_orders(orders):
     print("")
-    found = False
-    for i, (name, product, quantity) in enumerate(orders):
-        print(colored(f"\nOrder {i + 1}:", attrs=["bold"])) 
-        print("Customer Name: ", name, " \nProduct: ", product, " \nQuantity: ", quantity)
-        found = True
-    if not found:
+    if orders:
+        for i, (name, product, quantity) in enumerate(orders):
+            print(colored(f"\nOrder {i + 1}:", "grey", attrs=["bold"])) 
+            print("Customer Name: ", name, " \nProduct: ", product, " \nQuantity: ", quantity)
+    else:
         print("There are no orders yet.")
 
 def main():
